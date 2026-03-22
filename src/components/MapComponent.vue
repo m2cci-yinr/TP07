f0adc38740f797c0be00b66d6880ffbbb7a7e8d2
m2cci-firsovao <firsovao@etu.univ-grenoble-alpes.fr>
2026-03-22 21:28:42 +0100

    updated map component

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,52 +1,50 @@
 <template>
-    <div :id="props.mapId" class="map">
-        <!-- Your content here -->
-    </div>
+  <div :id="props.mapId" class="map"></div>
 </template>
 
 <script setup>
-import L from 'leaflet';
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import { MapManager } from '@/mapManager.js';
 
 const props = defineProps({
-    mapId: { type: String },
-    zoomLevel: { type: Number, default: 1 },
-    lat: { type: Number, default: 45.1875602 },
-    lon: { type: Number, default: 5.7357819 }
+  mapId:     { type: String },
+  zoomLevel: { type: Number, default: 1 },
+  lat:       { type: Number, default: 45.1875602 },
+  lon:       { type: Number, default: 5.7357819 }
 });
 
 let mapManager = null;
 
-onMounted(function () {
-    mapManager = new MapManager(props.mapId, props.lat, props.lon, props.zoomLevel, onSelectMarker);
+onMounted(() => {
+  mapManager = new MapManager(props.mapId, props.lat, props.lon, props.zoomLevel, onSelectMarker);
 });
 
 function createMarkers(lieux) {
-    mapManager.createMarkers(lieux);
+  mapManager.createMarkers(lieux);
 }
 
 function changeSelectedMarker(marker) {
-    mapManager.changeSelectedMarker(marker);
+  mapManager.changeSelectedMarker(marker);
 }
 
-// Lorsque l'utilisateur clique sur un marqueur un événément 'marker-selected'
-// est émis par le MapComponent
+// ── flyTo: animate map to any lat/lon/zoom (used by footer author buttons)
+function flyTo(lat, lon, zoom = 13) {
+  if (mapManager?.map) {
+    mapManager.map.flyTo([lat, lon], zoom, { animate: true, duration: 1.4 });
+  }
+}
+
 const emit = defineEmits(['marker-selected']);
-
 function onSelectMarker(location) {
-    emit('marker-selected', location);
+  emit('marker-selected', location);
 }
 
-// méthodes que le composant parent pourra utiliser
-defineExpose({ createMarkers, changeSelectedMarker });
-
+defineExpose({ createMarkers, changeSelectedMarker, flyTo });
 </script>
 
 <style scoped>
 .map {
-    width: 100%;
-    height: 500px;
-    border: solid 1px;
+  width: 100%;
+  height: 520px;
 }
 </style>
